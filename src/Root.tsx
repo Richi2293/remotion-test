9bf8b62f93a4e521c229ecec76c4942f1b7eccf2
Riccardo <info@riccardolorenzi.com>
2026-03-02 19:25:14 +0100

    initial blank project

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,43 +1,18 @@
 import "./index.css";
-import { Composition, getStaticFiles } from "remotion";
-import { AIVideo, aiVideoSchema } from "./components/AIVideo";
-import { FPS, INTRO_DURATION } from "./lib/constants";
-import { getTimelinePath, loadTimelineFromFile } from "./lib/utils";
+import { Composition } from "remotion";
+import { MyComposition } from "./Composition";
 
 export const RemotionRoot: React.FC = () => {
-  const staticFiles = getStaticFiles();
-  const timelines = staticFiles
-    .filter((file) => file.name.endsWith("timeline.json"))
-    .map((file) => file.name.split("/")[1]);
-
   return (
     <>
-      {timelines.map((storyName) => (
-        <Composition
-          id={storyName}
-          component={AIVideo}
-          fps={FPS}
-          width={1080}
-          height={1920}
-          schema={aiVideoSchema}
-          defaultProps={{
-            timeline: null,
-          }}
-          calculateMetadata={async ({ props }) => {
-            const { lengthFrames, timeline } = await loadTimelineFromFile(
-              getTimelinePath(storyName),
-            );
-
-            return {
-              durationInFrames: lengthFrames + INTRO_DURATION,
-              props: {
-                ...props,
-                timeline,
-              },
-            };
-          }}
-        />
-      ))}
+      <Composition
+        id="MyComp"
+        component={MyComposition}
+        durationInFrames={60}
+        fps={30}
+        width={1280}
+        height={720}
+      />
     </>
   );
 };
